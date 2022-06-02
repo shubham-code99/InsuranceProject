83fe44f38dba1a0bc4bd46bf69aff5a967009b23
shubham <shubham.verma.sv52@gmail.com>
2022-06-02 22:19:22 +0530

    Adding spring project

---
+++ insurance/src/test/java/com/insuranceproject/insurance/InsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insuranceproject.insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
